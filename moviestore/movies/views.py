76b4c062d8304f633fdd83b25e1b9810be91f6ae
johnclamy <johnclamy@gmail.com>
2025-03-01 08:55:25 +0000

    redirect the user depending on the request property in edit_review function

--- moviestore/movies/views.py
+++ moviestore/movies/views.py
@@ -52,3 +52,18 @@ def edit_review(request, id, review_id):
     
     if request.user != review.user:
         return redirect('movies.show', id=id)
+
+    if request.method == 'GET':
+        template_data = {}
+
+        template_data['title'] = 'Edit Review'
+        template_data['review'] = review
+        return render(request, 'movies/edit_review.html', {'template_data': template_data})
+    
+    elif request.method == 'POST' and request.POST['comment'] != '':
+        review = Review.objects.get(id=review_id)
+        review.comment = request.POST['comment']
+        review.save()
+        return redirect('movies.show', id=id)
+    else:
+        return redirect('movies.show', id=id)
