394ccef6f3af61c3d5c787d982a1898872f40f69
johnclamy <johnclamy@gmail.com>
2025-03-03 13:53:09 +0000

    define an index function for '/cart/views.py'

--- moviestore/cart/views.py
+++ moviestore/cart/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from movies.models import Movie
+from .utils import calculate_cart_total
+
+
+def index(request):
+    template_data = {}
+    cart_total = 0
+    movies_in_cart = []
+    cart = request.session.get('cart', {})
+    movie_ids = list(cart.keys())
+
+    if (movie_ids != []):
+        movies_in_cart = Movie.objects.filter(id__in=movie_ids)
+        cart_total = calculate_cart_total(cart, movies_in_cart)
+
+    template_data['title'] = 'Cart'
+    template_data['movies_in_cart'] = movies_in_cart
+    template_data['cart_total'] = cart_total
+
+    return render(request, 'cart/index.html', {'template_data': template_data})
 
 
 def add(request, id):
