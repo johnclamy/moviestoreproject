8076f20f9a232823b2454e796d0669b64bf19aaa
johnclamy <johnclamy@gmail.com>
2025-03-03 08:24:25 +0000

    define a add_to_cart func in '/cart/views.py'

--- moviestore/cart/views.py
+++ moviestore/cart/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from movies.models import Movie
 
-# Create your views here.
+
+def add(request, id):
+    get_object_or_404(Movie, id=id)
+    cart = request.session.get('cart', {})
+
+    cart[id] = request.POST['quantity']
+    request.session['cart'] = cart
+    return redirect('home.index')
