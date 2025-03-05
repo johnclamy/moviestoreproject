c2570fec94b01a5c85663ad50f5498c2fc5ba8b0
johnclamy <johnclamy@gmail.com>
2025-03-05 21:14:03 +0000

    define the clear func to remove all movie items from the cart

--- moviestore/cart/views.py
+++ moviestore/cart/views.py
@@ -28,3 +28,8 @@ def add(request, id):
     cart[id] = request.POST['quantity']
     request.session['cart'] = cart
     return redirect('cart.index')
+
+
+def clear(request):
+    request.session['cart'] = {}
+    return redirect['cart.index']
