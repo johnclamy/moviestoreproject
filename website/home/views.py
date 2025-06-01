87873a55d1771d08ff080d88114df44284b9433e
johnclamy <johnclamy@gmail.com>
2025-06-01 19:52:48 +0100

    add a data dict as a 3rd arg of the render func in index

--- website/home/views.py
+++ website/home/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'Welcome to Webfix | Home page'
+
+    return render(
+        request,
+        'home/index.html',
+        {
+            'template_data': template_data
+        }
+    )
 
 
 def about(request):
