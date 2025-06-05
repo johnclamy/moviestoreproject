c0d21b070bc022096134549197b8980eecf0d758
johnclamy <johnclamy@gmail.com>
2025-06-05 12:00:05 +0100

    add a movie rating field to the model

--- website/movies/models.py
+++ website/movies/models.py
@@ -5,6 +5,7 @@ class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.FloatField()
+    rating = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/')
 
