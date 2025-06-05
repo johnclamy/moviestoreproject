1324333d0a6ed42747586839308eaa1577daf10b
johnclamy <johnclamy@gmail.com>
2025-06-05 10:39:22 +0100

    rename img upload location folder to "movie_images/'

--- website/movies/models.py
+++ website/movies/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
-    image = models.ImageField(upload_to='posters/')
+    image = models.ImageField(upload_to='movie_images/')
 
     def __str__(self):
         return str(self.id) + ' - ' + self.name
