c6ad7ea6c4365e0b381d074ca6c8fed12c57353f
johnclamy <johnclamy@gmail.com>
2025-06-07 22:50:20 +0100

    add accounts url in the project-level urls.py file

--- website/website/urls.py
+++ website/website/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 
