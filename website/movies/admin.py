b13ce5c8992643ab17d09694fd3d255bb3350423
johnclamy <johnclamy@gmail.com>
2025-06-07 16:21:01 +0100

    order the movies list in the admin panel by movie name (title)

--- website/movies/admin.py
+++ website/movies/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Movie
 
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+
+
+admin.site.register(Movie, MovieAdmin)
