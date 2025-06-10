cee386332e71cf084732330856ded7f1a633cb5b
johnclamy <johnclamy@gmail.com>
2025-06-10 22:32:23 +0100

    define a create_review function in '/movies/views.py'

--- website/movies/views.py
+++ website/movies/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Movie
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Movie, Review
 
 
 def index(request):
@@ -37,4 +38,19 @@ def detail(request, id):
             'template_data': template_data
         }
     )
+
+
+@login_required
+def create_review(request, id):
+    if request.method == 'POST' and request.POST['comment'] != '':
+        movie = Movie.objects.get(id=id)
+        review = Review()
+
+        review.user = request.user
+        review.comment = request.POST['comment']
+        review.movie = movie
+        review.save()
+
+    return redirect('movies.detail', id=id)
+        
     
