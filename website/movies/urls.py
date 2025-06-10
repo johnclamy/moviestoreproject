f96286df9a2342d288ad8ebf86bbfa2430f7cac4
johnclamy <johnclamy@gmail.com>
2025-06-10 22:38:16 +0100

    configure the create review url in '/movies/urls.py'

--- website/movies/urls.py
+++ website/movies/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='movies.index'),
     path('<int:id>/', views.detail, name='movies.detail'),
+    path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
 ]
-
