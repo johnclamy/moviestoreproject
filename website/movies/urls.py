6b75e5e23f5589dbc2d2bbb21c4e83cbdb498f8f
johnclamy <johnclamy@gmail.com>
2025-06-12 11:53:00 +0100

    configure the edit_review url

--- website/movies/urls.py
+++ website/movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='movies.index'),
     path('<int:id>/', views.detail, name='movies.detail'),
     path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
+    path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
 ]
