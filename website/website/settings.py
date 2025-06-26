d857873732f11eaf6f3a362b20105c6460b07277
johnclamy <johnclamy@gmail.com>
2025-06-26 16:54:40 +0100

    include the template path of the base.html in templates attbr of settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +45,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'website/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
