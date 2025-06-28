63ce8197311141033c2ef1f8be00e48b8584be62
johnclamy <johnclamy@gmail.com>
2025-06-28 06:54:34 +0100

    set the title on home page

--- website/home/views.py
+++ website/home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    data = {}
+    data['title'] = 'Welcome to PUPS-R-US | Home page'
+
+    return render(request, 'home/index.html', { 'data': data })
 
 
 def about(request):
