4649fc29cc77d1b902e75eee9eec0934173cef52
johnclamy <johnclamy@gmail.com>
2025-07-21 22:36:38 +0100

    rename the about  function to about_us in '/home/views.py'

--- home/views.py
+++ home/views.py
@@ -5,5 +5,5 @@ def index(request):
     return render(request, 'home/index.html')
 
 
-def about(request):
-    return render(request, 'home/about.html')
+def about_us(request):
+    return render(request, 'home/about-us.html')
